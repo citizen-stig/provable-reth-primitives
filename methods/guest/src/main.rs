4588cb4f6a6bd00af9233569f246e419f3827635
Nikolai Golub <nikolay.v.golub@gmail.com>
2024-06-13 15:56:23 +0200

    Nonce + balance work

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,13 +1,15 @@
 use risc0_zkvm::guest::env;
+use alloy_primitives::{Address, B256};
+use reth_primitives::{Account, Signature, U256};
 
 fn main() {
-    // TODO: Implement your guest code here
+    let nonce: u64 = env::read();
 
-    // read the input
-    let input: u64 = env::read();
+    let account: Account = env::read();
 
-    // TODO: do something with the input
+    assert_eq!(nonce, account.nonce);
 
-    // write public output to the journal
-    env::commit(&input);
+    let output = nonce + account.balance.to::<u64>();
+
+    env::commit(&output);
 }
